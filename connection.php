889bb2ff202c67aaae666d2764a85bea4f57b0cf
lawlietsun <lawliet.sun@gmail.com>
2014-02-03 17:27:36 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,14 @@
 <?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "root";
-$mysql_database = "comp3013";
+// $mysql_hostname = "localhost";
+// $mysql_user = "root";
+// $mysql_password = "root";
+// $mysql_database = "comp3013";
+
+$mysql_hostname = "eu-cdbr-azure-west-b.cloudapp.net";
+$mysql_user = "bc303ee8a90e6f";
+$mysql_password = "21ca7bfd";
+$mysql_database = "yuesunuAKTN5vkXx";
+ 
 $prefix = "";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
