65bce343837707f79cb230283b732d0ef61c0905
lawlietsun <lawliet.sun@gmail.com>
2014-03-08 16:35:47 +0000

    update

--- regist/code_exec.php
+++ regist/code_exec.php
@@ -5,8 +5,20 @@ $fname=$_POST['fname'];
 $lname=$_POST['lname'];
 $email=$_POST['email'];
 $age=$_POST['age'];
-$password=$_POST['password'];
-mysql_query("INSERT INTO member(fname, lname, age, email, password)VALUES('$fname', '$lname', '$age', '$email', SHA1($password))");
-header("location: index.php?remarks=success");
-mysql_close($con);
+$creatpassword=$_POST['creatpassword'];
+$confirmpassword=$_POST['confirmpassword'];
+
+if ($creatpassword == $confirmpassword)
+{
+	$password = SHA1($_POST['creatpassword']);
+
+	mysql_query("INSERT INTO member(fname, lname, age, email, password)VALUES('$fname', '$lname', '$age', '$email', '$password')");
+	header("location: index.php?remarks=success");
+	mysql_close($bd);
+}
+
+else
+{
+	header("location: index.php?remarks=incorrect");
+}
 ?>
--- regist/index.php
+++ regist/index.php
@@ -1,50 +1,51 @@
 <html lang="en-US">
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="/comp3013/style.css">
 </head>
 <body>
   <div class="login-card">
     <h1>register</h1>
     <form name="reg" action="code_exec.php" onsubmit="return validateForm()" method="post">
-      <!-- <table> -->
       <tr>
         <td>       
           <div>
             <?php 
-            $remarks=$_GET['remarks'];
-            if ($remarks==null and $remarks=="")
+            $remarks = "";
+            if ( isset($_GET['remarks']) ) 
             {
-              echo '';
+              $remarks = $_GET['remarks'];
             }
-            if ($remarks=='success')
+
+            if ( $remarks == 'success' ) 
             {
               echo 'Registration Success';
             }
+
+            if ( $remarks == 'incorrect' ) 
+            {
+              echo 'Passwords inconsistent';
+            }
+
             ?>  
-          </div></td>
-        </tr>
+          </div>
+        </td>
+      </tr>
 
-        <form>
-          <input type="text" name="fname" placeholder="First Name" required="">
-          <input type="text" name="lname" placeholder="Last Name" required="">
-          <input type="text" name="age" placeholder="Age" required="">
-          <input type="email" name="email" placeholder="Username/Email" required="">
-          <input type="password" name="password" placeholder="Password" required="">
-          <input type="password" name="repassword" placeholder="Confirm Password" required="">
-          <button type="submit">Sign Up </button>
-        </form>
+      <form>
+        <input type="text" name="fname" placeholder="First Name" required="">
+        <input type="text" name="lname" placeholder="Last Name" required="">
+        <input type="text" name="age" placeholder="Age" required="">
+        <input type="email" name="email" placeholder="Username/Email" required="">
+        <input type="password" name="creatpassword" placeholder="Creat A Password" required="">
+        <input type="password" name="confirmpassword" placeholder="Confirm Password" required="">
+        <button type="submit">Sign Up </button>
+      </form>
 
-        <!-- <tr> -->
-        <!-- <td><div align="right"></div></td> -->
-        <!-- <td><input name="submit" type="submit" class="login login-submit" value="Sign Up" /></td> -->
-
-        <div class="login-help">
-          <a href="/comp3013/index.php">Already had an account</a>
-        </div>
-        <!-- </tr> -->
+      <div class="login-help">
+        <a href="/comp3013/index.php">Already had an account</a>
       </div>
-      <!-- </table> -->
     </form>
-  </body>
-  </html>
+  </div>
+</body>
+</html>
