48e3f5e0bce92163ae69cd1077f7149b35760739
JIAWEILONG <gary_0203@hotmail.com>
2014-03-13 02:17:06 +0800

    Update code_exec.php

--- regist/code_exec.php
+++ regist/code_exec.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-include('db/connection.php');
-
+include('connection.php');
 $fname=$_POST['fname'];
 $lname=$_POST['lname'];
 $email=$_POST['email'];
@@ -9,17 +8,26 @@ $age=$_POST['age'];
 $creatpassword=$_POST['creatpassword'];
 $confirmpassword=$_POST['confirmpassword'];
 
-if ($creatpassword == $confirmpassword)
+$result = mysql_query("SELECT email FROM member WHERE email = '$email'");
+if (mysql_num_rows($result) > 0)
 {
-	$password = SHA1($_POST['creatpassword']);
-
-	mysql_query("INSERT INTO member(fname, lname, age, email, password)VALUES('$fname', '$lname', '$age', '$email', '$password')");
-	header("location: index.php?remarks=success");
-	mysql_close($bd);
+	header("location: index.php?remarks=email");
 }
 
 else
 {
-	header("location: index.php?remarks=incorrect");
+	if ($creatpassword == $confirmpassword)
+	{
+		$password = SHA1($_POST['creatpassword']);
+
+		mysql_query("INSERT INTO member(fname, lname, age, email, password)VALUES('$fname', '$lname', '$age', '$email', '$password')");
+		header("location: index.php?remarks=success");
+		mysql_close($con);
+	}
+
+	else
+	{
+		header("location: index.php?remarks=passwordsincorrect");
+	}
 }
 ?>
