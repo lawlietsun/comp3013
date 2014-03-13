b98193a2a29ff7d245e7e791cd12c15b2a05e781
lawlietsun <lawliet.sun@gmail.com>
2014-03-13 17:24:59 +0000

    azure db bug fixed

--- regist/index.php
+++ regist/index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
   <div class="login-card">
-    <h1>register</h1>
+    <h1>Register</h1>
     <form name="reg" action="code_exec.php" method="post">
       <tr>
         <td>       
@@ -40,12 +40,12 @@
       </tr>
 
       <form>
-        <input type="text" name="fname" placeholder="First Name" required="">
-        <input type="text" name="lname" placeholder="Last Name" required="">
-        <input type="text" name="age" placeholder="Age" required="">
-        <input type="email" name="email" placeholder="Username/Email" required="">
-        <input type="password" name="creatpassword" placeholder="Creat A Password" required="">
-        <input type="password" name="confirmpassword" placeholder="Confirm Password" required="">
+        <input type="text" name="fname" placeholder="First Name" id="fname" required="">
+        <input type="text" name="lname" placeholder="Last Name" id="lname" required="">
+        <input type="text" name="age" placeholder="Age" id="age" required="">
+        <input type="email" name="email" placeholder="Username/Email" id="email" required="">
+        <input type="password" name="creatpassword" placeholder="Creat A Password" id="creatpassword" required="">
+        <input type="password" name="confirmpassword" placeholder="Confirm Password" id="confirmpassword" required="">
         <button type="submit">Sign Up </button>
       </form>
 
