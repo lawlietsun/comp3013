5c53c73740f7d5c71d20b2a385eb245cd6868606
JIAWEILONG <gary_0203@hotmail.com>
2014-03-15 01:12:35 +0800

    update
    更新了regist的条件

--- regist/code_exec.php
+++ regist/code_exec.php
@@ -1,42 +1,10 @@
 <?php
-// session_start();
-// include('connection.php');
-// $fname=$_POST['fname'];
-// $lname=$_POST['lname'];
-// $email=$_POST['email'];
-// $age=$_POST['age'];
-// $creatpassword=$_POST['creatpassword'];
-// $confirmpassword=$_POST['confirmpassword'];
-
-// $result = mysql_query("SELECT email FROM member WHERE email = '$email'");
-// if (mysql_num_rows($result) > 0)
-// {
-// 	header("location: index.php?remarks=email");
-// }
-
-// else
-// {
-// 	if ($creatpassword == $confirmpassword)
-// 	{
-// 		$password = SHA1($_POST['creatpassword']);
-
-// 		mysql_query("INSERT INTO member(fname, lname, age, email, password)VALUES('$fname', '$lname', '$age', '$email', '$password')");
-// 		header("location: index.php?remarks=success");
-// 		mysql_close($bd);
-// 	}
-
-// 	else
-// 	{
-// 		header("location: index.php?remarks=passwordsincorrect");
-// 	}
-// }
-
+session_start();
 
 $host = "eu-cdbr-azure-west-b.cloudapp.net";
 $user = "b0be1b17ba7186";
 $pwd = "62ebd3b9";
 $db = "comp301A7bEecu9x";
-
     // Connect to database.
 try {
 	$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
@@ -46,26 +14,48 @@ catch(Exception $e){
 	die(var_dump($e));
 }
     // Insert registration info
-if(!empty($_POST)) {
-	try {
-		$fname = $_POST['fname'];
-		$lname = $_POST['lname'];
-		$email = $_POST['email'];
-		$age = $_POST['age'];
-		$password = sha1($_POST['creatpassword']);
-        // Insert data
-		$sql_insert = "INSERT INTO member(fname, lname, age, email, password) VALUES (?,?,?,?,?)";
-		$stmt = $conn->prepare($sql_insert);
-		$stmt->bindValue(1, $fname);
-		$stmt->bindValue(2, $lname);
-		$stmt->bindValue(3, $age);
-		$stmt->bindValue(4, $email);
-		$stmt->bindValue(5, $password);
-		$stmt->execute();
+if(!empty($_POST)) 
+{
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$email = $_POST['email'];
+	$age = $_POST['age'];
+	$creatpassword=$_POST['creatpassword'];
+	$confirmpassword=$_POST['confirmpassword'];
+	
+	$result = mysql_query("SELECT email FROM member WHERE email = '$email'");
+	$rownumber = mysql_num_rows($result);
+	if ($rownumber > 0)
+	{
+		if ($creatpassword == $confirmpassword)
+		{
+			try 
+			{
+				$password = sha1($_POST['creatpassword']);
+       	 	// Insert data
+				$sql_insert = "INSERT INTO member(fname, lname, age, email, password) VALUES (?,?,?,?,?)";
+				$stmt = $conn->prepare($sql_insert);
+				$stmt->bindValue(1, $fname);
+				$stmt->bindValue(2, $lname);
+				$stmt->bindValue(3, $age);
+				$stmt->bindValue(4, $email);
+				$stmt->bindValue(5, $password);
+				$stmt->execute();
+			}
+			catch(Exception $e) {
+				die(var_dump($e));
+			}
+			header("location: index.php?remarks=success");
+		}
+		else
+		{
+			header("location: index.php?remarks=passwordsincorrect");
+		}
 	}
-	catch(Exception $e) {
-		die(var_dump($e));
+	else
+	{
+		header("location: index.php?remarks=email");
 	}
-	echo "<h3>Your're registered!</h3>";
+	
 }
 ?>
