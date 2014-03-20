3b55735fa4453808f93db62546aa7a38fe47ba60
JIAWEILONG <gary_0203@hotmail.com>
2014-03-21 02:13:51 +0800

    search

---
+++ search2/search_exec.php
@@ -0,0 +1,24 @@
+<?php
+// session_start();
+require_once('../db/connection.php');
+$message="";
+if(count($_POST)>0) 
+{
+    $result = mysql_query("SELECT * FROM member WHERE fname LIKE '%" . $_POST["name"] . "%' OR lname LIKE '%" . $_POST["name"] . "%'");
+    $rownumber = mysql_num_rows($result);
+    if($rownumber > 0)
+    {
+        // header("Location: index.php?remarks=match");
+        while($rownumber = mysql_fetch_array($result))
+        {
+            echo $rownumber['fname'];
+            echo " ";
+            echo $rownumber['lname']."<br />";
+        }
+    }
+    else
+    {
+        header("Location: index.php?remarks=unmatch");
+    }
+}
+?>
