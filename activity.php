a88055475900afb016c5d91c4cb7ba23a2060acc
lawlietsun <lawliet.sun@gmail.com>
2014-03-20 18:07:36 +0000

    link update

--- activity.php
+++ activity.php
@@ -11,7 +11,7 @@
     <ul id="main_menu">
       <li><a href="home.php" title="home">Home</a></li>
       <li><a href="profile.php" title="Profile">Profile</a></li>
-      <li><a href="friends.php" title="Friends">Friends</a></li>
+      <li><a href="friends/index.php" title="Friends">Friends</a></li>
       <li class="selected"><a href="activity.php" title="activity">Activity</a></li>
       <li><a href="search.php" title="search">Search</a></li>
       <li><a href="/index.php" title="logout">Log Out</a></li>
