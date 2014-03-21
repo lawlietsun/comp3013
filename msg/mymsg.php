e3d39035ab07987705e9f6fcd9ed5ed95c556fa3
lawlietsun <lawliet.sun@gmail.com>
2014-03-21 21:51:21 +0000

    tt

--- msg/mymsg.php
+++ msg/mymsg.php
@@ -9,7 +9,7 @@ include('../db/connection.php');
 		<?php
 			$user = $_SESSION['mem_id']; //$user = $_SESSION['username'];
 			// echo "$user";
-			$qu = mysql_query("SELECT * from messages where touser = $user");
+			// $qu = mysql_query("SELECT * from messages where touser = $user");
 			$qu = mysql_query("SELECT fname, lname, message from member, messages where mem_id = (SELECT fromuser from messages where touser = $user)");
 			// if (mysql_num_rows($qu) > 0) {
 				while ($row = mysql_fetch_array($qu)) {
