3fb273ff522b3d61656853634b56a326ea016485
lawlietsun <lawliet.sun@gmail.com>
2014-03-21 04:54:17 +0000

    css

--- friends/ffriends.php
+++ friends/ffriends.php
@@ -1,6 +1,6 @@
 
 <link rel="stylesheet" type="text/css" href="style.css">
-  <!-- <link rel="stylesheet" type="text/css" href="../css/homestyle.css"> -->
+  <link rel="stylesheet" type="text/css" href="../css/homestyle.css">
 
 <?php
 // session_start();
